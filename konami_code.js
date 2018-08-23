402729ff6cd6f57d3ce78081112abe9c4cfb8b17
Yuri Tomikawa <yuritomikawa@gmail.com>
2018-08-23 23:11:07 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,25 +1,46 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 //  up-up-down-down-left-right-left-right-B-A
 
-function init() {
-  // Write your JavaScript code inside the init() function
-  // tracker keeps track of where we are in the array
-  let tracker = 0 // note: this needs to be 'let' since we're changing this value
-  const body = document.querySelector('body')
-  body.addEventListener('keydown', (event) => {
-    if (event.which === code[tracker]) {
-      console.log('Success!')
-      tracker++
-      if(tracker === code.length) {
-        alert('You found it!')
-        let paragraph = document.createElement('p')
-        body.append("yay great job!", paragraph)
-      }
-    } else {
-      console.log('you messed up :(')
-      tracker = 0
-    }
+// function init() {
+//   // Write your JavaScript code inside the init() function
+//   // tracker keeps track of where we are in the array
+//   let tracker = 0 // note: this needs to be 'let' since we're changing this value
+//   const body = document.querySelector('body')
+//   body.addEventListener('keydown', (event) => {
+//     if (event.which === code[tracker]) {
+//       console.log('Success!')
+//       tracker++
+//       if(tracker === code.length) {
+//         alert('You found it!')
+//         let paragraph = document.createElement('p')
+//         body.append("yay great job!", paragraph)
+//       }
+//     } else {
+//       console.log('you messed up :(')
+//       tracker = 0
+//     }
+//   }
+//   )
+// }
+// init()
+
+function onKeyDownHandler(event) {
+  console.log(tracker)
+  const key = parseInt(event.detail||event.which)
+  if(tracker === code.length) {
+    event.preventDefault()
+    body.append('Great Job', paragraph)
+    return
   }
-  )
+  if(key === code[tracker]) {
+    console.log('Success')
+    tracker++;
+    if(tracker=== code.length) {
+      window.alert('You found it') 
+    }
+  else {
+    console.log('You messed up')
+    tracker=0;
+  }
+  }
 }
-init()
